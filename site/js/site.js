88a2a4e8dc059dd4ba1e1a584b95f24508bb219e
José L. Patiño <jose.lpa@gmail.com>
2016-04-30 18:25:17 +0100

    Site contents are now rendered by a custom directive.

--- site/js/site.js
+++ site/js/site.js
@@ -1,15 +1,22 @@
 (function() {
     var app = angular.module('site', []);
 
-    app.controller('ContentController', function() {
-        this.panel = 1;
+    app.directive('contentPanels', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'content_panels.html',
+            controller: function() {
+                this.panel = 1;
 
-        this.selectPanel = function(setPanel) {
-            this.panel = setPanel;
-        };
+                this.selectPanel = function(setPanel) {
+                    this.panel = setPanel;
+                };
 
-        this.isSelected = function(checkPanel) {
-            return this.panel == checkPanel;
+                this.isSelected = function(checkPanel) {
+                    return this.panel == checkPanel;
+                };
+            },
+            controllerAs: 'content'
         };
     });
 })();
